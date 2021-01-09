077432b525ef3ca30a696a910a59cffbd89a8dbf
amirvahedix <amirvahedix@gmail.com>
2021-01-09 11:21:27 +0330

    draw snake to screen

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let lastRenderTime = 0
 import {SNAKE_SPEED, update as updateSnake, draw as drawSnake} from './snake.js '
+const gameBoard = document.querySelector('#game-board')
 
 function main(currentTime){
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
@@ -9,7 +10,7 @@ function main(currentTime){
     lastRenderTime = currentTime
 
     updateSnake()
-    drawSnake()
+    drawSnake(gameBoard)
 }
 
 window.requestAnimationFrame(main)
--- snake.js
+++ snake.js
@@ -1,10 +1,20 @@
 export const SNAKE_SPEED = 2
-
+const SNAKE_BODY = [
+    { x: 11 , y: 11 },
+    { x: 12 , y: 11 },
+    { x: 13 , y: 11 }
+]
 
 export function update() {
     console.log('update snake')
 }
 
 export function draw(gameBoard) {
-    console.log('draw snake')
+    SNAKE_BODY.forEach(segmant => {
+        const snakeElemnt = document.createElement('div')
+        snakeElemnt.style.gridRowStart = segmant.x
+        snakeElemnt.style.gridColumnStart = segmant.y
+        snakeElemnt.classList.add('snake')
+        gameBoard.appendChild(snakeElemnt)
+    })
 }
