1fdd5fe0ff2328bf689463d42f2b68f95853edbe
amirvahedix <amirvahedix@gmail.com>
2021-01-09 14:23:55 +0330

    move snake with keyboard arrows

--- snake.js
+++ snake.js
@@ -1,21 +1,15 @@
 import { getInputDirection } from "./input.js"
 
 export const SNAKE_SPEED = 2
-const SNAKE_BODY = [
-    { x: 11 , y: 11 },
-    { x: 12 , y: 11 },
-    { x: 13 , y: 11 },
-    { x: 14 , y: 11 },
-    { x: 15 , y: 11 },
-    { x: 16 , y: 11 },
-]
+const SNAKE_BODY = [{ x: 11 , y: 11 }]
 
 export function update() {
+    const inputDirection = getInputDirection()
     for(let i = SNAKE_BODY.length - 2; i >= 0; i--){
         SNAKE_BODY[i + 1] = { ...SNAKE_BODY[i] }
     }
-    SNAKE_BODY[0].x += 0
-    SNAKE_BODY[0].y += 1
+    SNAKE_BODY[0].x += inputDirection.x
+    SNAKE_BODY[0].y += inputDirection.y
 }
 
 export function draw(gameBoard) {
